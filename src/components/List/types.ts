4fb9f2bf67c2e91ecd291eb6bdd10f1a6af182ef
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-03-04 22:45:28 -0300

    refactor: moved list types into its own folder

--- src/components/List/types.ts
+++ src/components/List/types.ts
@@ -1,1 +1,8 @@
-export type {CardProps} from '../Card/types';
+import {CardProps} from '../Card/types';
+import {ListRenderItem} from 'react-native';
+
+export type ListProps = {
+  data: CardProps[];
+};
+
+export type ListRenderItemReturn = ListRenderItem<CardProps>;
