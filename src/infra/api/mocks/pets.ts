f3c310e8f468e923506ad0ab9826a5dd1c7c7139
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-03-04 22:43:22 -0300

    refactor: created mock for categories and pets

---
+++ src/infra/api/mocks/pets.ts
@@ -0,0 +1,60 @@
+const pets = [
+  {
+    petType: 'dog',
+    title: 'Buddy',
+    subtitle: 'Shiba Inu',
+    age: '1 Year old',
+    gender: 'boy',
+    photoSource: require('../../../assets/buddy.png'),
+  },
+  {
+    petType: 'cat',
+    title: 'Cooter',
+    subtitle: 'Maine Coon',
+    age: '6 Months old',
+    gender: 'girl',
+    photoSource: require('../../../assets/cooter.png'),
+  },
+  {
+    petType: 'bird',
+    title: 'asda',
+    subtitle: 'Maine Coon',
+    age: '6 Months old',
+    gender: 'girl',
+    photoSource: require('../../../assets/buddy.png'),
+  },
+  {
+    petType: 'all',
+    title: 'das',
+    subtitle: 'Maine Coon',
+    age: '6 Months old',
+    gender: 'girl',
+    photoSource: require('../../../assets/cooter.png'),
+  },
+  {
+    petType: 'all',
+    title: 'asdCooterasdsasda',
+    subtitle: 'Maine Coon',
+    age: '6 Months old',
+    gender: 'girl',
+    photoSource: require('../../../assets/buddy.png'),
+  },
+  {
+    petType: 'all',
+    title: 'Coasoterasdsasda',
+    subtitle: 'Maine Coon',
+    age: '6 Months old',
+    gender: 'girl',
+    photoSource: require('../../../assets/cooter.png'),
+  },
+  {
+    petType: 'all',
+    title: 'Cooterassdsasda',
+    subtitle: 'Maine Coon',
+    age: '6 Months old',
+    gender: 'girl',
+    photoSource: require('../../../assets/buddy.png'),
+  },
+];
+
+export default pets;
