97138756d98e2a0d0e288dce5b181dd3d787c071
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-03-04 22:48:03 -0300

    style: removed logs

--- src/infra/api/routes/categories/index.tsx
+++ src/infra/api/routes/categories/index.tsx
@@ -6,6 +6,5 @@ import * as Types from './types';
 export const getAll = (): Types.CategoriesReturn[] => categories;
 
 export const getOne = (category: Types.Category): Types.CategoryReturn => {
-  console.log('category: ', category);
   return pets.filter((pet) => pet.petType === category);
 };
