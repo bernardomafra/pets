6859247790bf86974228b18057d1e2705f8c858b
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-03-04 22:45:41 -0300

    refactor: removed list data mock

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 // import * as Styles from './styles';
 
-import {FlatList, ListRenderItem} from 'react-native';
+import {FlatList} from 'react-native';
 import Card from '../Card';
 
 import * as Types from './types';
 
-const List = () => {
-  const renderItem: ListRenderItem<Types.CardProps> = ({item}) => (
+const List: React.FC<Types.ListProps> = ({data}) => {
+  const renderItem: Types.ListRenderItemReturn = ({item}) => (
     <Card
       photoSource={item.photoSource}
       petType={item.petType}
@@ -18,33 +18,6 @@ const List = () => {
       gender={item.gender}
     />
   );
-
-  const data: Types.CardProps[] = [
-    {
-      petType: 'dog',
-      title: 'Buddy',
-      subtitle: 'Shiba Inu',
-      age: '1 Year old',
-      gender: 'boy',
-      photoSource: require('../../assets/buddy.png'),
-    },
-    {
-      petType: 'cat',
-      title: 'Cooter',
-      subtitle: 'Maine Coon',
-      age: '6 Months old',
-      gender: 'girl',
-      photoSource: require('../../assets/cooter.png'),
-    },
-    {
-      petType: 'cat',
-      title: 'Cooterasd',
-      subtitle: 'Maine Coon',
-      age: '6 Months old',
-      gender: 'girl',
-      photoSource: require('../../assets/cooter.png'),
-    },
-  ];
 
   return (
     <FlatList
