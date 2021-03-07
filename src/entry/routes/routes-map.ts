9052d17d0a5dd298f0a67cdd6e51f72c74afa507
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-03-07 16:36:57 -0300

    feat: add headerShown in routeMap to make it optional

--- src/entry/routes/routes-map.ts
+++ src/entry/routes/routes-map.ts
@@ -15,7 +15,7 @@ const routesMap: Types.IRouteMap[] = [
     key: 'info',
     name: 'Info',
     title: 'More information',
-    headerShown: true,
+    headerShown: false,
     component: Info,
     initialParams: {petId: 0},
   },
