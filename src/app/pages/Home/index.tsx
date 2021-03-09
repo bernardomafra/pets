03df78f1411ba76c86204fc6dc8e8b7ce1ce77ec
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-03-08 22:45:50 -0300

    chore: removed unnecessary AppProvider wrapper in Home.tsx

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -3,23 +3,25 @@ import React from 'react';
 import * as Styles from './styles';
 import * as GlobalStyles from '../../../../global-styles';
 
-import AppProvider from '../../../entry/context';
-
 import CustomStatusBar from '../../components/CustomStatusBar';
 import Header from '../../components/Header';
 import Body from '../../components/Body';
+import {AppContext} from '../../../entry/context';
 
 const Home = () => {
+  const {customStatusBarColor} = React.useContext(AppContext);
+
   return (
-    <AppProvider>
-      <Styles.Screen>
-        <CustomStatusBar backgroundColor="#FFF" barStyle="dark-content" />
-        <GlobalStyles.Container>
-          <Header />
-          <Body />
-        </GlobalStyles.Container>
-      </Styles.Screen>
-    </AppProvider>
+    <Styles.Screen>
+      <CustomStatusBar
+        backgroundColor={customStatusBarColor}
+        barStyle="dark-content"
+      />
+      <GlobalStyles.Container>
+        <Header />
+        <Body />
+      </GlobalStyles.Container>
+    </Styles.Screen>
   );
 };
 
