fe5cd63ec97cb31f21365ee62ccf263b5f37cedb
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-03-08 22:49:08 -0300

    style: fixed font-family in Badge

--- src/app/components/Badge/styles.ts
+++ src/app/components/Badge/styles.ts
@@ -17,6 +17,9 @@ export const Badge = styled.TouchableOpacity`
 export const BadgeText = styled.Text`
   font-family: 'MontserratAlternates-Medium';
   font-size: 17px;
+  font-style: normal;
+  font-weight: 600;
+
   color: ${(props: Types.BadgeStyleProps) =>
     props.selected ? '#FFF' : '#777777'};
 `;
